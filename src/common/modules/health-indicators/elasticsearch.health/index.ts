b6dcf8fd9f32cc702511bbf89ae1198c53924f31
RXRD <lilixinxin@protonmail.com>
2024-01-26 13:27:02 +0700

    Update index.ts

--- src/common/modules/health-indicators/elasticsearch.health/index.ts
+++ src/common/modules/health-indicators/elasticsearch.health/index.ts
@@ -1,29 +1,19 @@
-import { GCloudSecretManagerService } from '@debionetwork/nestjs-gcloud-secret-manager';
 import { Module } from '@nestjs/common';
 import { ElasticsearchModule } from '@nestjs/elasticsearch';
 import { TerminusModule } from '@nestjs/terminus';
 import { ElasticsearchHealthIndicator } from './elasticsearch.health.indicator';
-import { keyList } from '../../../secrets';
 
 @Module({
   imports: [
     TerminusModule,
     ElasticsearchModule.registerAsync({
-      inject: [GCloudSecretManagerService],
       useFactory: async (
-        gCloudSecretManagerService: GCloudSecretManagerService<keyList>,
       ) => {
         return {
-          node: gCloudSecretManagerService
-            .getSecret('ELASTICSEARCH_NODE')
-            .toString(),
+          node: process.env.ELASTICSEARCH_NODE.toString(),
           auth: {
-            username: gCloudSecretManagerService
-              .getSecret('ELASTICSEARCH_USERNAME')
-              .toString(),
-            password: gCloudSecretManagerService
-              .getSecret('ELASTICSEARCH_PASSWORD')
-              .toString(),
+            username: process.env.ELASTICSEARCH_USERNAME.toString(),
+            password: process.env.ELASTICSEARCH_PASSWORD.toString(),
           },
         };
       },
