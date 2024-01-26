2f69565e045bd127be925cbabc5e1f1289114975
RXRD <lilixinxin@protonmail.com>
2024-01-26 11:24:32 +0700

    fix index.ts

--- src/common/modules/caches/index.ts
+++ src/common/modules/caches/index.ts
@@ -1,23 +1,20 @@
 import { CacheModule, Module } from '@nestjs/common';
 import { CachesService } from './caches.service';
 import * as redisStore from 'cache-manager-redis-store';
-import { GCloudSecretManagerService } from '@debionetwork/nestjs-gcloud-secret-manager';
 import { keyList } from '../../secrets';
+
+require('dotenv').config(); // eslint-disable-line
 
 @Module({
   imports: [
     CacheModule.registerAsync({
-      inject: [GCloudSecretManagerService],
       useFactory: async (
-        gCloudSecretManagerService: GCloudSecretManagerService<keyList>,
       ) => {
         return {
           store: redisStore,
-          host: gCloudSecretManagerService.getSecret('REDIS_HOST').toString(),
-          port: gCloudSecretManagerService.getSecret('REDIS_PORT').toString(),
-          auth_pass: gCloudSecretManagerService
-            .getSecret('REDIS_PASSWORD')
-            .toString(),
+          host: process.env.REDIS_HOST.toString(),
+          port: process.env.REDIS_PORT.toString(),
+          auth_pass: process.env.REDIS_PASSWORD.toString(),
         };
       },
     }),
